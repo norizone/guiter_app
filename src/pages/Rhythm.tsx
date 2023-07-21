578a2e64e74cf5249fad66264620c5c957950ddf
minami <minami.takanori.1113@gmail.com>
2023-07-21 18:24:55 +0900

    modal展開時のバグ修正

--- src/pages/Rhythm.tsx
+++ src/pages/Rhythm.tsx
@@ -1,11 +1,18 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+
 import { PrimaryHeadline } from "@/components/headlines/PrimaryHeadline";
 import { InputBpmArea } from "@/features/rhythm/components/InputBpmArea";
 import { SelectRhythmTypeArea } from "@/features/rhythm/components/SelectRhythmTypeArea";
 import { PrimaryInputsWrap } from "@/components/wraps/PrimaryInputsWrap";
 import { SelectBeat } from "@/features/rhythm/components/SelectBeat";
+import { useModal } from "@/hooks/useModal";
 
 export const Rhythm = () => {
+  const {closeModal} = useModal()
+  useEffect(()=>{
+    return () => closeModal();
+  },[])
   return (
     <motion.main
       initial={{ opacity: 0, y: 0, zIndex: 0 }}
